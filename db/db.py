6842af488d94b8a956162be82f1ac424592878db
Sudheesh901 <sudheeshkrj@gmail.com>
2025-11-18 11:40:28 +0530

    New Databased created

--- db/db.py
+++ db/db.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DB_USER= "ragsudheesh"
-DB_PASS= "Sudhi%40901"  # encoded @ as %40
+DB_USER= "sudheeshcom"
+DB_PASS= "sudheesh798"
 DB_HOST = "localhost"
 DB_PORT = "5432"
-DB_NAME = "medibot"
+DB_NAME = "CompilotDB"
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
